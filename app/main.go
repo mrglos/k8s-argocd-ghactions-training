5063c00d5bf11ba08737e62653f44cbf75483ee9
Marek Glos <mrglos>
2026-03-05 14:39:10 +0100

    Add go application and Dockerfile

---
+++ app/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// Accept the exact same -text flag our Helm chart is currently passing
+	textPtr := flag.String("text", "Default Go Hello", "Text to display")
+	flag.Parse()
+
+	// Create a simple web server
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "%s\n", *textPtr)
+	})
+
+	// Listen on port 5678, just like the old http-echo container
+	fmt.Println("Go Server starting on port 5678...")
+	http.ListenAndServe(":5678", nil)
+}
